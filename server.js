0b9631aa5b48a2dd7d7289b6c9308aca3c12fe66
Ayush Sharma <ayush.s.sharma04@gmail.com>
2025-01-11 14:27:30 +0530

    fix(cocckies & Cors) 3

--- server.js
+++ server.js
@@ -23,17 +23,11 @@ dotenv.config();
 const app = express();
 
 //middlewares
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "https://sn-music-student-frontend.vercel.app", "https://sn-music-student-frontend-git-e441d8-ayushsharma2004s-projects.vercel.app/", "https://sn-music-admin-frontend.vercel.app"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "https://sn-music-student-frontend.vercel.app", "https://sn-music-student-frontend-git-e441d8-ayushsharma2004s-projects.vercel.app/", "https://sn-music-admin-frontend.vercel.app", "https://sn-music-admin-frontend.vercel.app/auth/login"];
 
 // CORS middleware
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true); // Allow request if origin is in the list or null (non-browser request)
-    } else {
-      callback(new Error("Not allowed by CORS")); // Block request otherwise
-    }
-  },
+  origin: allowedOrigins,
   credentials: true, // Allow credentials like cookies
 }));
 
